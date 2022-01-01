84b935cf53b828403ce053064546fad7813ed3ba
Eric Foard <foard265@gmail.com>
2022-01-01 10:22:28 -0800

    basic structure with stubs

--- src/dominion_simulator/core.clj
+++ src/dominion_simulator/core.clj
@@ -4,3 +4,26 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn is-game-over
+  "given a game state, determine if the game is over"
+  [game-state]
+  false)
+
+(defn get-winner
+  "given a game state, determine the winner of the game"
+  [game-state]
+  (first (:players game-state)))
+
+(defn do-turn
+  "given a game state, simulate turn and return next game state"
+  [game-state]
+  game-state)
+
+(defn simulate-game
+  "main function for simulating a game of dominion"
+  [game-state]
+  (if (is-game-over (:cards game-state))
+    (get-winner (:players game-state))
+    (let [next-game-state (do-turn game-state)]
+      (simulate-game next-game-state))))
