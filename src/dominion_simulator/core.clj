60534bd8073b6059cc8f4edddcf8901e9e68b347
Eric Foard <foard265@gmail.com>
2022-01-01 14:22:09 -0800

    player turn functions

--- src/dominion_simulator/core.clj
+++ src/dominion_simulator/core.clj
@@ -55,54 +55,65 @@
   ;; TODO
   game-state)
 
-(defn do-player-action-phase
+(defn discard-hand
   [game-state player-index]
   ;; TODO
   game-state)
+
+
+(defn do-player-action-phase
+  [game-state player-index]
+  (let [player    (get (:players game-state) player-index)
+        action-fn (:action-fn player)]
+    (action-fn player game-state)))
 
 
 (defn do-player-buy-phase
   [game-state player-index]
-  ;; TODO
-  game-state)
-
+  (let [player (get (:players game-state) player-index)
+        buy-fn (:buy-fn player)]
+    (buy-fn player game-state)))
 
 (defn do-player-cleanup-phase
-  [game-state player-index]
-  ;; TODO
-  game-state)
-
-(defn do-player-turn
   "given a game state, simulate turn of a single player and return next game state"
   [game-state player-index]
   (let [player (get (:players game-state) player-index)]
     (-> game-state
-        (do-player-action-phase player-index)
-        (do-player-buy-phase player-index)
-        (do-player-cleanup-phase player-index))))
+        (discard-hand player-index)
+        (maybe-shuffle-draw-deck player-index)
+        (draw-hand player-index))))
+
+(defn do-player-turn
+"given a game state, simulate turn of a single player and return next game state"
+[game-state player-index]
+(let [player (get (:players game-state) player-index)]
+  (-> game-state
+      (do-player-action-phase player-index)
+      (do-player-buy-phase player-index)
+      (do-player-cleanup-phase player-index))))
 
 (defn do-turn
-  "given a game state, simulate turn of all players and return next game state"
-  [game-state]
-  (loop [players           (:players game-state)
-         curr-player-index 0]
-    (if (= curr-player-index (- (count players) 1))
-      game-state
-      (recur (do-player-turn game-state curr-player-index) (+ curr-player-index 1)))))
+"given a game state, simulate turn of all players and return next game state"
+[game-state]
+(loop [players           (:players game-state)
+       curr-player-index 0]
+  (if (= curr-player-index (- (count players) 1))
+    game-state
+    (recur (do-player-turn game-state curr-player-index) (+ curr-player-index 1)))))
 
 (defn setup-game
-  "perform required setup prior to player turns"
-  [game-state]
-  ;; TODO
-  game-state)
+"perform required setup prior to player turns"
+[game-state]
+;; TODO
+game-state)
 
 (defn simulate-game
-  "main function for simulating a game of dominion"
-  [game-state]
-  (if (is-game-over game-state)
-    (get-winner game-state)
-    (let [next-game-state (do-turn game-state)]
-      (simulate-game next-game-state))))
+"main function for simulating a game of dominion"
+[game-state]
+(if (is-game-over game-state)
+  (get-winner game-state)
+  (let [next-game-state (do-turn game-state)]
+    (simulate-game next-game-state))))
 
 
 ;; (simulate-game (setup-game (init-game-state default-players default-kingdom-cards default-victory-cards default-treasure-cards )))
